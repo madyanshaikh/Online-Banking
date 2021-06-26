1a5a5e429c871af5031fcd139ac8f0db4dfb30b3
Sheikh-Farzaib <sfarzaib@gmail.com>
2021-06-26 18:22:41 +0500

    Added Hide/Show Button

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,26 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+declare var $: any;
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
-}) 
+})
 export class LoginFormComponent implements OnInit {
-
+  ngOnInit(): void{
+    $(document).ready(function() {
+      $("#show_hide_password a").on('click', function(event: { preventDefault: () => void; }){
+          event.preventDefault();
+          if($('#show_hide_password input').attr("type") == "text"){
+              $('#show_hide_password input').attr('type', 'password');
+              $('#show_hide_password i').addClass( "fa-eye-slash" );
+              $('#show_hide_password i').removeClass( "fa-eye" );
+          }else if($('#show_hide_password input').attr("type") == "password"){
+              $('#show_hide_password input').attr('type', 'text');
+              $('#show_hide_password i').removeClass( "fa-eye-slash" );
+              $('#show_hide_password i').addClass( "fa-eye" );
+          }
+      });
+  });
+  }
   username = ''
   password = ''
 
-  constructor(private snackbar:MatSnackBar,private router:Router) { }
+  constructor(private snackbar: MatSnackBar, private router: Router) { }
 
 
-  ngOnInit(): void {
-  }
+ 
   onlogin() {
-    if(this.username=='admin' && this.password=='admin123')
-    {
-      sessionStorage.setItem("isLogedIn","true")
+    if (this.username == 'admin' && this.password == 'admin123') {
+      sessionStorage.setItem("isLogedIn", "true")
       this.router.navigate(['index1'])
     }
     else {
