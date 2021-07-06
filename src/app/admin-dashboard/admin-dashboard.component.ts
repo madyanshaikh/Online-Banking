85a92bc170abb9cbad1ab847724f81a7aa87ba7d
madyanshaikh <57924238+madyanshaikh@users.noreply.github.com>
2021-07-06 14:36:00 +0500

    user Pulled

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,36 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import {trigger, state, style, animate, transition} from '@angular/animations';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 declare var $: any;
+
+
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css'],
-  animations: [
+  // animations: [
+
+  //   trigger('rotatedState', [
+  //     state('default', style({ transform: 'rotate(0)' })),
+  //     state('rotated', style({ transform: 'rotate(-180deg)' })),
+  //     transition('rotated => default', animate('1000ms ease-out')),
+  //     transition('default => rotated', animate('400ms ease-in'))
+  //   ])
+  // ]
   
-    trigger('rotatedState', [
-      state('default', style({ transform: 'rotate(0)' })),
-      state('rotated', style({ transform: 'rotate(-180deg)' })),
-      transition('rotated => default', animate('1000ms ease-out')),
-      transition('default => rotated', animate('400ms ease-in'))
-    ])
-  ]
- 
+
 })
 export class AdminDashboardComponent implements OnInit {
-  state: string = 'default';
-  rotate() {
-    this.state = (this.state === 'default' ? 'rotated' : 'default');
-}
+  toggleClass: any;
+  
+    // state: string = 'default';
+    // check:boolean =false;
+    // rotate() {
+    //   this.state = (this.state === 'default' ? 'rotated' : 'default');
+    //   // this.check=!this.check;
+    // }
   constructor() { }
 
   ngOnInit(): void {
+    
+   $(document).ready(function(){
+    $(".hover-only").mouseenter(function(){
+        $('i').css('color','slategray');
+    });
+    $(".hover-only").mouseleave(function(){
+      $('i').css('color','lightgray');
+  });
 
-    // $(".flip").click(()=>{
+})
 
-    //     $(".btn-toggle1").toggleClass('rotated')
 
-    // });
+  
+    
+      
+      
+    
   }
+  addClass(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
+  
 
 }
 
