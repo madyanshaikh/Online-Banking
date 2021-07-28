452435e1d4ffa3f114e619249a2261180d9ff1c1
MuhammadSubhanRaza <msraza302@gmail.com>
2021-07-28 13:07:49 +0500

    Error resolved
    Error

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-<<<<<<< HEAD
-=======
 
 
 
->>>>>>> 7050222b9718aa437fa9f6b37b1b12b3029c5d86
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -21,15 +18,9 @@ export class WelcomeComponent implements OnInit {
     
   }
 welcome(){
-<<<<<<< HEAD
-  sessionStorage.setItem("isLogedIn", "true")
-  this.router.navigate(['login-form'])
-}
-=======
  
   sessionStorage.setItem("isLogedIn", "true")
   this.router.navigate(['login-form'])
 }
 
->>>>>>> 7050222b9718aa437fa9f6b37b1b12b3029c5d86
 }
